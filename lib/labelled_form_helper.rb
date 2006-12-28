2378be87d3763008b768853df8957088959d76a8
remco <remco@35f5d0d5-a825-0410-916a-a08118deaa0c>
2006-12-28 15:27:56 +0000

    r896@fina084:  remco | 2006-12-28 16:27:41 +0100  typo in date and datetime selects, dont label check_boxes, hiddenfields and radio_buttons, expose label method
    git-svn-id: http://svn.remvee.net/plugins/labelify@10 35f5d0d5-a825-0410-916a-a08118deaa0c

--- lib/labelled_form_helper.rb
+++ lib/labelled_form_helper.rb
@@ -12,10 +12,10 @@ module LabelledFormHelper
 
   # Form build for +form_for+ method which includes labels with all form fields.
   class LabelledFormBuilder < ActionView::Helpers::FormBuilder
-    %w(text_field password_field file_field check_box radio_button text_area hidden_field select
-              datetime_field date_field collection_select country_select time_zone_select).each do |method|
+    %w(text_field password_field file_field text_area select datetime_select date_select
+                    collection_select country_select time_zone_select).each do |method|
       define_method(method.to_sym) do |method_name, *args|
-        label_tag(method_name) + @template.send(method.to_sym, object_name, method_name, *args)
+        label(method_name) + @template.send(method.to_sym, object_name, method_name, *args)
       end
     end
 
@@ -29,17 +29,17 @@ module LabelledFormHelper
       %Q@<input #{options.map { |k,v| "#{k}=\"#{h v.to_s}\"" }.join(' ')}/>@
     end
 
-  private
-    def label_tag(method_name)
+    def label(method_name, options = {})
       column = object.class.columns_hash[method_name.to_s]
       %Q@
-        <label for="#{object_name}_#{method_name}">
+        <label for="#{object_name}_#{method_name}" #{options.map { |k,v| "#{k}=\"#{h v.to_s}\"" }.join(' ')}>
           #{column ? column.human_name : method_name.to_s.humanize}
           #{error_messages(method_name)}
         </label>
       @
     end
 
+  private
     def error_messages(method_name)
       if messages = object.errors.on(method_name)
         messages = messages.to_sentence if messages.respond_to? :to_sentence
